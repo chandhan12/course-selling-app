e4ea0bad094234ba88560306010d02528622ff47
chandhan <chandhan.g0401@gmail.com>
2024-10-29 19:01:13 +0530

    added admin router

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const {userRouter}=require('./routes/user.js');
 const { courseRouter } = require("./routes/course.js");
+const { adminRouter} = require("./routes/admin.js");
 
 const app=express();
 
-app.use('/user',userRouter);
-app.use('/course',courseRouter);
+app.use('/api/user',userRouter);
+app.use('/api/course',courseRouter);
+app.use('/api/admin',adminRouter)
 
 app.listen(3000,() =>{
     console.log("server is started and running at port 3000")
---
+++ routes/admin.js
@@ -0,0 +1,47 @@
+const {Router} =require("express")
+
+const adminRouter=Router();
+
+
+adminRouter.post('/signup' ,(req,res) => {
+
+    res.json({
+        msg:" admin signup endpoint"
+    })
+})
+
+adminRouter.post('/signin' ,(req,res) => {
+
+    res.json({
+        msg:" admin signin endpoint"
+    })
+})
+
+adminRouter.post("/course", (req,res) =>{
+
+    res.json({
+        msg:"create course endpoint"
+    })
+})
+
+//to update the course
+
+adminRouter.put("/course", (req,res) =>{
+
+    res.json({
+        msg:"update course endpoint"
+    })
+})
+
+
+adminRouter.get("/course/bulk", (req,res) =>{
+
+    res.json({
+        msg:"all the courses you have created"
+    })
+})
+
+
+module.exports={
+    adminRouter:adminRouter
+}
