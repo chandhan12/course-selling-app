4cf092e624c5cb470f07d655b1127f7960031623
chandhan <chandhan.g0401@gmail.com>
2024-10-30 11:50:12 +0530

    pushed mongo url my mistake so changed the password and kept it in .env file

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const express=require("express");
 const {userRouter}=require('./routes/user.js');
 const { courseRouter } = require("./routes/course.js");
 const { adminRouter} = require("./routes/admin.js");
-
+require('dotenv').config();
 const mongoose=require("mongoose");
 
-
-mongoose.connect("mongodb+srv://chandhangorlannagari:Chandhan$9398641656@cluster0.9feoq.mongodb.net/course-selling-app",)
-.then(() => console.log("connected to mongoDB"))
-.catch((err) =>console.log(err.message))
 
 
 const app=express();
@@ -17,6 +13,12 @@ app.use('/api/user',userRouter);
 app.use('/api/course',courseRouter);
 app.use('/api/admin',adminRouter)
 
-app.listen(3000,() =>{
-    console.log("server is started and running at port 3000")
-})
+const main=async () =>{
+        await mongoose.connect(process.env.MONGO_URL,)
+        console.log("connected to mongoDB")
+    app.listen(3000,() =>{
+        console.log("server is started and running at port 3000")
+    })
+}
+
+main();
